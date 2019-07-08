b65ae2d64e8636879651306c3c97380ea5c91ffe
ankitjain28may <ankitjain28may77@gmail.com>
2019-07-08 23:14:23 +0530

    Adding travis

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/ankitjain28may/module-submodule.git
+ls
+cd module-submodule
+git submodule update --recursive --remote --merge
+ls submodule
+cat submodule/README.md
+git status
+git add .
+git commit -m"Travis update submodule $TRAVIS_BUILD_NUMBER"
+git push origin master
